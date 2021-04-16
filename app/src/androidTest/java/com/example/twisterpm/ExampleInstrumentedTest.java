67a13500485c0f5ca22daf2006bc081b9ec7059e
Kimon Kremizas <k.kremizas@gmail.com>
2021-04-16 15:30:13 +0200

    Added an Espresso UI test

--- app/src/androidTest/java/com/example/twisterpm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/twisterpm/ExampleInstrumentedTest.java
@@ -1,13 +1,34 @@
 package com.example.twisterpm;
 
+
 import android.content.Context;
 
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Random;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -22,5 +43,30 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("com.example.twisterpm", appContext.getPackageName());
+    }
+
+
+    @Rule
+    public ActivityScenarioRule rule = new ActivityScenarioRule<>(AllMessagesActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
+
+    @After
+    public void tearDown() {
+        Intents.release();
+    }
+
+    @Test
+    public void testIfPostedMessageIsFoundInRecyclerViewAndNavigateToIt(){
+        // random int makes sure the message is not duplicate
+        Random rand = new Random();
+        int randomInt = rand.nextInt(10000);
+        onView(withId(R.id.newMessageEditText)).perform(typeText("This is my espresso test number " + randomInt));
+        onView(withId(R.id.postNewMessageButton)).perform(click());
+        onView(withId(R.id.messagesRecyclerView)).check(matches(hasDescendant(withText("This is my espresso test number " + randomInt))));
+        onView(withId(R.id.messagesRecyclerView)).perform(RecyclerViewActions.scrollTo(hasDescendant(withText("This is my espresso test number " + randomInt))));
     }
 }
